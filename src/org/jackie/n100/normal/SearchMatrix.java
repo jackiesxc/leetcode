ffc3cc43136fd63c74ffd106fa49dc45af10fb7d
SXC <SXC@DESKTOP-48ELCN5.mshome.net>
2020-09-06 23:58:31 +0800

    74. 搜索二维矩阵

--- src/org/jackie/n100/normal/SearchMatrix.java
+++ src/org/jackie/n100/normal/SearchMatrix.java
@@ -12,8 +12,55 @@ import org.junit.Test;
 
 public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-    	return false;
+        if ( matrix.length==0 )
+            return false;        
+        int m = matrix[0].length;
+        int indexRow = countIndexRow(matrix, target, m);
+    	return isExist(matrix[indexRow], target, 0, m-1);
     }
+    
+    private boolean isExist( int[] matrix, int target, int start, int end )
+    {
+        while ( start <= end )
+        {
+        	int index = (start + end)/2;
+            if ( target > matrix[index] )
+            {
+                start = index+1;
+            }
+            else if ( target < matrix[index]) {
+                end = index-1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int countIndexRow( int[][] matrix, int target, int m)
+    {
+        int n = matrix.length;
+    	int start= 0; 
+    	int end = n-1;
+        while ( end > start )
+        {
+        	int index = (start + end)/2;
+        	
+            if ( target < matrix[index][0] )
+            {
+            	end = index-1;
+            }
+            else if ( target > matrix[index][m-1] ){
+            	start = index+1;
+            }
+            else {
+            	return index;
+			}
+        }
+        return start;
+    }
+    
 	@Test
     public void test1() {
 		SearchMatrix fun = new SearchMatrix();
@@ -24,5 +71,17 @@ public class SearchMatrix {
     public void test2() {
 		SearchMatrix fun = new SearchMatrix();
 		assertEquals(false, fun.searchMatrix(new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,50}}, 13));
+    }
+	
+	@Test
+    public void test3() {
+		SearchMatrix fun = new SearchMatrix();
+		assertEquals(true, fun.searchMatrix(new int[][] {{1}}, 1));
+    }
+	
+	@Test
+    public void test4() {
+		SearchMatrix fun = new SearchMatrix();
+		assertEquals(true, fun.searchMatrix(new int[][] {{1},{3}}, 3));
     }
 }
